7fc173226bc4cb545aa73471874b9cabf0b7bca1
Abdessadek <abdessadekhm@gmail.com>
2025-04-12 09:12:50 +0100

    Add Basic Landing page

---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-contact',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './contact.component.html',
+  
+})
+export class ContactComponent {
+  contactForm = {
+    name: '',
+    email: '',
+    company: '',
+    message: ''
+  };
+
+  onSubmit() {
+    // Handle form submission logic here
+    console.log('Form submitted:', this.contactForm);
+  }
+} 
