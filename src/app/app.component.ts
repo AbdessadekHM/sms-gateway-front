d319dcdff73036a550c2f0e69b9d6370164dcb17
Khabir <hakim1khabir2@gmail.com>
2025-04-28 11:16:19 +0100

    add auth (login, logout and register)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
 import { FooterComponent } from "./footer/footer.component";
 import { HomeComponent } from "./home/home.component";
+import {AuthService} from './services/auth.service';
+import {AsyncPipe, JsonPipe, NgIf} from '@angular/common';
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
     templateUrl: './app.component.html',
 })
 export class AppComponent {
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,42 @@
+import {inject, Injectable} from '@angular/core';
+import Keycloak from 'keycloak-js';
+import {Router} from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private keycloak = inject(Keycloak);
+  private router = inject(Router);
+  private authenticated = false;
+
+  public async login() {
+    // log the current url
+    console.log('Current URL:', window.location.href);
+    await this.keycloak.login({
+        redirectUri: window.location.origin
+    });
+  }
+
+  public async logout() {
+    await this.keycloak.logout({
+      redirectUri: window.location.origin
+    });
+  }
+
+  public isAuthenticated(): boolean {
+    return !!this.keycloak.authenticated;
+  }
+
+  public async getProfile(): Promise<Keycloak.KeycloakProfile> {
+    return this.keycloak.loadUserProfile();
+  }
+
+  public async register() {
+    await this.keycloak.register({
+      redirectUri: window.location.href
+    })
+  }
+
+}
