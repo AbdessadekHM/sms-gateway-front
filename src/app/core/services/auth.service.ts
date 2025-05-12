1f89b1d3e3097939c7e41901176cf929b2e59cad
pc <fahlaouimohammed@gmail.com>
2025-05-12 21:26:04 +0100

    auth

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -4,20 +4,44 @@ import { Router } from '@angular/router';
 import {User} from '../../dto/user';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {AuthResponse} from '../../dto/AuthResponse';
 
 @Injectable({
   providedIn: 'root',
+
 })
 export class AuthService {
-  public http=inject(HttpClient);
-  public  url="http://localhost:8000/"
-  public token:string="";
-
-  public login(username:any, password:any) {
+  public  url="http://127.0.0.1:7001/"
+  public token:string | undefined="";
+  public valid=false
+  public router=inject(Router)
+  public http=inject(HttpClient)
+  public login(username:string, password:string) {
   let endPoint="auth/login"
 
     let user:User={username:username , password:password }
-    this.http.post<AuthService>(this.url+endPoint,{user}).subscribe(e => {this.token=e.token;console.log(e.token)});
+    let req=this.http.post<AuthResponse>(this.url+endPoint,{username:username , password:password})
+      .subscribe({next:e=> {
+          this.token = e.token;
+          if (this.token != "" && this.token != null) {
+            sessionStorage.setItem("accessToken", <string>e.token)
+            console.log(e.token);
+            this.router.navigateByUrl('/')
+
+          }},
+
+
+          error: e=>{   this.router.navigateByUrl('/register')}
+
+
+
+
+
+
+
+      });
+  return this.valid;
+
 
   }
 
