c0ffe05c9aacbaf59e22af0b4e4944dff40cdb9a
Abdessadek <abdessadekhm@gmail.com>
2025-05-18 18:19:04 +0100

    Refactor models

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,10 +1,13 @@
 import { inject, Injectable } from '@angular/core';
 import Keycloak from 'keycloak-js';
 import { Router } from '@angular/router';
-import {User} from '../../dto/user';
+
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {AuthResponse} from '../../dto/AuthResponse';
+import { User } from '../models/user';
+import { AuthResponse } from '../models/AuthResponse';
+import { AuthenticatedLinks, Link, UnauthenticatedLinks } from '../models/NavLinks';
+
 
 @Injectable({
   providedIn: 'root',
@@ -13,19 +16,37 @@ import {AuthResponse} from '../../dto/AuthResponse';
 export class AuthService {
   public  url="http://127.0.0.1:7001/"
   public token:string | undefined="";
+
   public valid=false
+
   public router=inject(Router)
+
   public http=inject(HttpClient)
+
+  private isAuth = false; ;
+
+  private navBarLinks: Link[] = UnauthenticatedLinks;
+
+
+
+
+
   public login(username:string, password:string) {
+
+
+
   let endPoint="auth/login"
 
-    let user:User={username:username , password:password }
-    let req=this.http.post<AuthResponse>(this.url+endPoint,{username:username , password:password})
+
+    let req=this.http.post<AuthResponse>(this.url + endPoint,{ username, password})
       .subscribe({next:e=> {
           this.token = e.token;
           if (this.token != "" && this.token != null) {
-            sessionStorage.setItem("accessToken", <string>e.token)
+            // sessionStorage.setItem("accessToken", <string>e.token)
+            localStorage.setItem("accessToken", <string>e.token)
             console.log(e.token);
+            this.isAuth = true;
+            this.navBarLinks = AuthenticatedLinks;
             this.router.navigateByUrl('/dashboard')
 
           }},
@@ -40,18 +61,22 @@ export class AuthService {
 
 
       });
-  return this.valid;
+    return this.valid;
 
 
   }
 
   public  logout() {
+    this.isAuth = false;
+    localStorage.removeItem('accessToken');
+    this.navBarLinks = UnauthenticatedLinks;
+    this.router.navigateByUrl('/login');
 
 
   }
 
   public isAuthenticated(): boolean {
-    return true
+    return this.isAuth;
   }
 
   public  getProfile() {
@@ -59,5 +84,9 @@ export class AuthService {
 
   public  register() {
 
+  }
+
+  get NavBarLinks(): Link[] {
+    return this.navBarLinks;
   }
 }
